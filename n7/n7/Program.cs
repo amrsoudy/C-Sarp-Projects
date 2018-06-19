9fc213e0a99eff08df1b4fcb810b003bb87458e0
Amr <32444508+amrsoudy@users.noreply.github.com>
2018-06-19 13:45:35 -0400

    correct the ex7 with  TRYParce
    Try

--- n7/n7/Program.cs
+++ n7/n7/Program.cs
@@ -19,28 +19,81 @@ namespace n7
 
         static void Main(string[] args)
         {
-            Console.WriteLine("ecrire le no d,Article");
-            fact F = new fact();
-            F.no = int.Parse(Console.ReadLine());
+            int j;
+            Console.WriteLine("ecrire le no total  des Articles");
+            
+             bool res0 = int.TryParse(Console.ReadLine(),out  j);
 
-            Console.WriteLine("ecrire le nom d article ");
-            F.nom = Console.ReadLine();
+            while (!res0) {
+                Console.WriteLine("ecrire le no total  des Articles correct SVP");
 
-            Console.WriteLine("ecrire le NoTotal d article ");
+                res0 = int.TryParse(Console.ReadLine(), out j);
 
-            F.NoTotal = int.Parse(Console.ReadLine());
+            }
 
-            Console.WriteLine("ecrire le prix Svp");
-            F.prix = double.Parse(Console.ReadLine());
+            if (j > 0)
+            {
+
+                for (int i = 0; i < j; i++)
+                {
+
+                    fact F = new fact();
+                    Console.WriteLine("ecrire le no d,Article  ");
+
+                    bool res = int.TryParse(Console.ReadLine(), out F.no);
+                    while (!res)
+                    {
+                        Console.WriteLine("ecrire le no d,Article autre fois ");
+                        res = int.TryParse(Console.ReadLine(), out F.no);
+                    }
+
+                    Console.WriteLine("ecrire le nom d article ");
+                    F.nom = Console.ReadLine();
+
+                    Console.WriteLine("ecrire le NoTotal d article ");
+
+                    bool res2 = int.TryParse(Console.ReadLine(), out F.NoTotal);
+
+                    while (!res2)
+                    {
+                        Console.WriteLine("ecrire le NoTotal d article  correst svp ");
+                        res2 = int.TryParse(Console.ReadLine(), out F.NoTotal);
+
+                    }
+
+                    Console.WriteLine("ecrire le prix Svp");
+                   bool res3 = double.TryParse(Console.ReadLine(), out F.prix);
+
+                    while (!res3) {
+                        Console.WriteLine("ecrire le prix Svp correct  svp ");
+                        res3 = double.TryParse(Console.ReadLine(), out F.prix);
+
+                    }
 
 
-            Utils.remplier(F);
-            Utils.afficher();
+                    Utils.remplier(F);
+                    Utils.afficher();
+
+
+                }
+
+                Console.WriteLine("*****************************************************");
+                Utils.afficherTotal();
+
+            }
+            else
+            {
+
+
+                Console.WriteLine(" le  no  d article doit etre positive plus de 0 ");
+
+            }
 
 
 
 
-            Utils.afficherTotal();
+
+
 
 
         }
---
+++ n8/n8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace n8
+{
+
+    struct date
+    {
+        public int aneee;
+        public int mois;
+        public int jour;
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+        }
+    }
+}
