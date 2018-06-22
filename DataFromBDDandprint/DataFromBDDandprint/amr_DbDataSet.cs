e33687d436869623ac8e7c462a080b2188cfd007
amrsoudy <32444508+amrsoudy@users.noreply.github.com>
2018-06-22 16:42:26 -0400

    up

---
+++ DataFromBDDandprint/DataFromBDDandprint/amr_DbDataSet.cs
@@ -0,0 +1,20 @@
+﻿namespace DataFromBDDandprint
+{
+
+
+    public partial class amr_DbDataSet
+    {
+    }
+}
+namespace DataFromBDDandprint {
+    
+    
+    public partial class amr_DbDataSet {
+    }
+}
+namespace DataFromBDDandprint {
+    
+    
+    public partial class amr_DbDataSet {
+    }
+}
