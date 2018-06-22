166b5c14b4c037b2fd29c26cd45f9e9bd0143876
amrsoudy <32444508+amrsoudy@users.noreply.github.com>
2018-06-22 13:00:17 -0400

    up

---
+++ PanierAchatProj/PanierAchatProj/PanierAchat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanierAchatProj
+{
+    class PanierAchat
+    {
+        private String _nom;
+        public String Nom { get; set; }
+
+        private string _noItem;
+        private string _nomItem;
+        private string _tailleItem;
+        private Decimal _prixItem;
+
+        public string codeItem { get { return _noItem;}  set { _noItem = value; } }
+        public String nameItem { get { return _nomItem; } set { _nomItem = value; } }
+        public string taillItem { get { return _tailleItem; } set { _tailleItem = value; } }
+
+        public Decimal priceItem { get { return _prixItem; } set { _prixItem = value; } }
+
+        public PanierAchat(string noItem,string nomItem,string tailleItem,Decimal prixItem)
+        {
+
+            codeItem = noItem;
+            nameItem = nomItem;
+            taillItem = tailleItem;
+            priceItem = prixItem;
+
+        }
+
+    }
+}
