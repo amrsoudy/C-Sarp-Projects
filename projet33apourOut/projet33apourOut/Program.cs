772df3bda7a948bb85f1e7509319d56ee6cafd14
amrsoudy <32444508+amrsoudy@users.noreply.github.com>
2018-06-27 07:00:46 -0500

    up

---
+++ projet33apourOut/projet33apourOut/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet33apourOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int varOut ;
+            double resOut ;
+            MethodeDeTestOut(out varOut, out resOut);
+            Console.WriteLine(varOut);
+            Console.ReadKey();
+
+            
+        }
+
+        private static void MethodeDeTestOut(out int varOut, out double resOut)
+        {
+
+            varOut = 60;
+            resOut = 45;
+           // Console.WriteLine(varOut);
+          //  Console.WriteLine(resOut);
+
+
+
+        }
+    }
+}
