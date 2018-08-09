9835b615d13b960b9ae693b77cfffab8777c0e4d
Amr <32444508+amrsoudy@users.noreply.github.com>
2018-08-09 16:30:10 -0400

    up

---
+++ eventEtLesDelegates/eventEtLesDelegates/Observer1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eventEtLesDelegates
+{
+    class Observer1
+    {
+        public void EcrireEcran(String mess) {
+
+
+            Console.WriteLine("Observateur 1 : " + mess);
+        }
+    }
+}
---
+++ eventEtLesDelegates/eventEtLesDelegates/Publisher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eventEtLesDelegates
+{
+    public delegate void MonDelegue(String input);
+    class Publisher
+    {
+       
+       public int temp { get; set; }
+        public event MonDelegue ValeurTemperatureChange;
+
+        public void notifier( int val) {
+
+            temp = val;
+            onValeurTemperatureChanger("Demarrage" + temp);
+    }
+
+        private void onValeurTemperatureChanger(string message)
+        {
+            if (ValeurTemperatureChange != null) {
+
+                ValeurTemperatureChange(message);
+            }
+        }
+    }
+}
+
+
