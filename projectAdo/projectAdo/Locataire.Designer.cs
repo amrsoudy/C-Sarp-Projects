9f71d17fa787b9018a37a0d0a89a66d434ea3d74
Amr <32444508+amrsoudy@users.noreply.github.com>
2018-09-03 14:52:58 -0400

    up

--- projectAdo/projectAdo/Locataire.Designer.cs
+++ projectAdo/projectAdo/Locataire.Designer.cs
@@ -50,6 +50,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSourceLocataire)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.amrDBDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridLocataire)).BeginInit();
@@ -192,6 +193,8 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.DataSource = this.bindingSourceLocataire;
+            this.comboBox1.DisplayMember = "Statutmariage";
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(121, 104);
             this.comboBox1.Name = "comboBox1";
@@ -228,11 +231,21 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(689, 358);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Enregistrer";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Locataire
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -273,6 +286,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
