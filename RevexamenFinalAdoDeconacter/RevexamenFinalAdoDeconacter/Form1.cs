2b5ca48e39a01d5e62b79b71fac1bf207a1796ad
Amr <32444508+amrsoudy@users.noreply.github.com>
2018-09-06 21:21:29 -0400

    up

---
+++ RevexamenFinalAdoDeconacter/RevexamenFinalAdoDeconacter/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RevexamenFinalAdoDeconacter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void iT_CLIENTBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.iT_CLIENTBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.amrDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: cette ligne de code charge les données dans la table 'amrDBDataSet.IT_CLIENT'. Vous pouvez la déplacer ou la supprimer selon les besoins.
+            this.iT_CLIENTTableAdapter.Fill(this.amrDBDataSet.IT_CLIENT);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            try
+            {
+
+                this.iT_CLIENTBindingSource.EndEdit();
+                this.iT_CLIENTTableAdapter.Fill(this.amrDBDataSet.IT_CLIENT);
+
+
+                this.iT_CLIENTTableAdapter.Update(this.amrDBDataSet.IT_CLIENT);
+
+
+                //    this.iT_CLIENTTableAdapter.Insert(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+                //    this.iT_CLIENTTableAdapter.Fill(this.amrDBDataSet.IT_CLIENT);
+                //    this.iT_CLIENTTableAdapter.Update(this.amrDBDataSet.IT_CLIENT);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            this.iT_CLIENTBindingSource.RemoveCurrent();
+            this.iT_CLIENTTableAdapter.Update(this.amrDBDataSet.IT_CLIENT);
+
+
+            foreach (Control c in panel1.Controls)
+            {
+
+                
+                //if (c is TextBox)
+                //{
+
+                //    c.Text = "";
+                //}
+                //if (c is ComboBox)
+                //{
+
+                //    c.TabIndex = 0;
+
+                //}
+
+
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                this.iT_CLIENTTableAdapter.SearchName(this.amrDBDataSet.IT_CLIENT, textBox6.Text);
+               
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+
+
+        
+
+        }
+
+
+
+        private void searchNameToolStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void nameToolStripTextBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.iT_CLIENTTableAdapter.insert(this.amrDBDataSet.IT_CLIENT);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void iT_CLIENTDataGridView_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,53 @@
+﻿
+CREATE TABLE IT_CLIENT (
+Num_cli INTEGER PRIMARY KEY  IDENTITY ,
+nom VARCHAR(20),
+prenom VARCHAR(20),
+Address VARCHAR(50),
+ville VARCHAR(30),
+CONSTRAINT pr_nom_prenom UNIQUE (nom,prenom),
+
+
+);
+
+INSERT INTO dbo.IT_CLIENT
+(
+    nom,
+    prenom,
+    Address,
+    ville
+)
+VALUES
+(   'Pattre', -- nom - varchar(20)
+    'Brod', -- prenom - varchar(20)
+    '14, avenue du Parc', -- Address - varchar(50)
+    'LaMuque'  -- ville - varchar(30)
+    )
+
+	INSERT INTO dbo.IT_CLIENT
+(
+    nom,
+    prenom,
+    Address,
+    ville
+)
+VALUES
+(   'Juna', -- nom - varchar(20)
+    'Mina', -- prenom - varchar(20)
+    '554, Blvd des reveurs,', -- Address - varchar(50)
+    'Bartue sur le Lac'  -- ville - varchar(30)
+    )
+
+		INSERT INTO dbo.IT_CLIENT
+(
+    nom,
+    prenom,
+    Address,
+    ville
+)
+VALUES
+(   'Icano', -- nom - varchar(20)
+    'Sal', -- prenom - varchar(20)
+    '4555, Chemin du Lac', -- Address - varchar(50)
+    'LaMuque'  -- ville - varchar(30)
+    )
